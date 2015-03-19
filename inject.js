8cdbdf00b4a67855e1838f783d9c432aae86081f
John Williams <johncomposed@gmail.com>
2015-03-19 18:32:23 -0400

    Cleaning up master

---
+++ inject.js
@@ -0,0 +1,16 @@
+
+/*
+
+Checkpage
+  if login page (http://myneu.neu.edu/cp/home/displaylogin)
+  if main page
+
+Mainpage
+  destroyExistingPage
+  Add ng stuff
+  Add new page
+
+Loginpage
+  Minor redesign
+
+*/
